655ef45a6f2c5beaec599b73beded534b3294d28
Micha G <25931746+src-mgra@users.noreply.github.com>
2017-08-16 22:13:53 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ZWR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.K = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(449, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(66, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X1,
+            this.X2,
+            this.X3,
+            this.X4,
+            this.X5,
+            this.X6,
+            this.ZWR,
+            this.K});
+            this.dataGridView1.Location = new System.Drawing.Point(24, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(394, 199);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // X1
+            // 
+            this.X1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X1.HeaderText = "X1";
+            this.X1.Name = "X1";
+            this.X1.Width = 45;
+            // 
+            // X2
+            // 
+            this.X2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X2.HeaderText = "X2";
+            this.X2.Name = "X2";
+            this.X2.Width = 45;
+            // 
+            // X3
+            // 
+            this.X3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X3.HeaderText = "X3";
+            this.X3.Name = "X3";
+            this.X3.Width = 45;
+            // 
+            // X4
+            // 
+            this.X4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X4.HeaderText = "X4";
+            this.X4.Name = "X4";
+            this.X4.Width = 45;
+            // 
+            // X5
+            // 
+            this.X5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X5.HeaderText = "X5";
+            this.X5.Name = "X5";
+            this.X5.Width = 45;
+            // 
+            // X6
+            // 
+            this.X6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.X6.HeaderText = "X6";
+            this.X6.Name = "X6";
+            this.X6.Width = 45;
+            // 
+            // ZWR
+            // 
+            this.ZWR.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ZWR.HeaderText = "=";
+            this.ZWR.Name = "ZWR";
+            this.ZWR.Width = 38;
+            // 
+            // K
+            // 
+            this.K.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.K.HeaderText = "K";
+            this.K.Name = "K";
+            this.K.Width = 39;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 234);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GLS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(441, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(89, 234);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ergebnisse";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Berechne";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Demo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(156, 275);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(106, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "gestaffelte Matrix";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 253);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Speichern";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(440, 275);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Laden";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 274);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Lösche";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(319, 253);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(34, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Location = new System.Drawing.Point(95, 262);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(25, 13);
+            this.Info.TabIndex = 12;
+            this.Info.Text = "Info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 302);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "SolveGLS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ZWR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn K;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label Info;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,521 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+
+        CalcGLSTyp GLS = new CalcGLSTyp();   // GLS-Objekt istanzieren
+        int Max;
+
+        // *** Methoden Array-Tranfer
+        public void ClearGrid()
+        {
+            int i;
+            int j;
+
+            try
+            {
+                for (i = 0; i <= Max; i++)
+                {
+                    for (j = 0; j <= Max; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = " ";
+                    }
+                    dataGridView1.Rows[i].Cells[7].Value = " ";
+                }
+            }
+            catch
+            {
+            }
+
+            dataGridView1.Rows.Clear();
+            
+        }
+
+        public void ArrayToGrid(int Max)
+        {
+            int i;
+            int j;
+
+            for (i = 0; i <= Max; i++)
+            {
+                for (j = 0; j <= Max; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = GLS.GetXVal(i, j);
+                }
+                dataGridView1.Rows[i].Cells[7].Value = GLS.GetKVal(i);
+            }
+        }
+
+        public void GridToArray(int Max)
+        {
+            int i;
+            int j;
+
+            try
+            {
+                for (i = 0; i <= Max; i++)
+                {
+                    for (j = 0; j <= Max; j++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[j].Value != " ")
+                        {
+                            GLS.SetXVal(i, j, Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value));
+                        }
+                    }
+                    if (dataGridView1.Rows[i].Cells[7].Value != " ")
+                    {
+                        GLS.SetKVal(i, Convert.ToDouble(dataGridView1.Rows[i].Cells[7].Value));
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void GridToFile(int Max)
+        {
+            int i;
+            int j;
+            string fName;
+            
+            fName="c:\\Temp\\GLS.xml";
+            // Datei zum schreiben öffnen
+            SaveFileDialog openFileDialog1 = new SaveFileDialog();
+            openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                fName = openFileDialog1.FileName;
+                        
+            //MessageBox.Show("Datei: "+tmpFile);
+            FileStream fs = new FileStream(fName,FileMode.Create,FileAccess.Write);
+            TextWriter tw = new StreamWriter(fs);
+
+            tw.WriteLine("<GLS>");
+            for (i = 0; i <= Max; i++)
+            {
+                tw.WriteLine("<Line>");
+                for (j = 0; j <= Max; j++)
+                {
+                    tw.WriteLine("<XVal>");
+                    tw.WriteLine(dataGridView1.Rows[i].Cells[j].Value);
+                    tw.WriteLine("</XVal>");
+                }
+                tw.WriteLine("<KVal>");
+                tw.WriteLine(dataGridView1.Rows[i].Cells[7].Value);
+                tw.WriteLine("</KVal>");
+                tw.WriteLine("</Line>");
+            }
+            tw.WriteLine("</GLS>");
+            tw.Close();         // schließen der Datei
+
+            // Dateiname andrucken
+            textBox1.Text = fName;
+       
+        }
+
+        public int FileToGrid(string fName)
+        {
+            int i;
+            int j;            
+            string fLine;
+
+            fLine = "";
+            if (fName == "")
+            {
+                // Datei zum lesen öffnen
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    fName = openFileDialog1.FileName;
+            }
+            //MessageBox.Show("Datei: "+tmpFile);
+            FileStream fs = new FileStream(fName, FileMode.Open, FileAccess.Read);
+            TextReader tr = new StreamReader(fs);
+
+            i = -1; j = -1;
+            fLine = tr.ReadLine();    // Startzeile lesen
+            while (fLine != null)
+            {
+                fLine = tr.ReadLine();  // Zeilenstart lesen
+
+                if ((fLine == "<Line>") || (fLine == "<XVal>") || (fLine == "<KVal>"))
+                {
+                    if (fLine == "<Line>")
+                    {
+                        i = i + 1;      // Row zählen
+                        if (i+2>dataGridView1.Rows.Count) {
+                            dataGridView1.Rows.Add();
+                        }
+                        fLine = tr.ReadLine();   // Wertstart lesen
+                    }
+                    
+                    if (fLine == "<XVal>")
+                    {
+                        j = j + 1; //Elemente zählen
+                        fLine = tr.ReadLine();
+                        dataGridView1.Rows[i].Cells[j].Value = fLine;
+                    }
+                    if (fLine == "<KVal>")
+                    {
+                        fLine = tr.ReadLine();
+                        dataGridView1.Rows[i].Cells[7].Value = fLine;
+                        fLine = tr.ReadLine();  // Zeilenende mit lesen
+                        j = -1;                 // j zurücksetzen
+                    }
+                    fLine = tr.ReadLine();   // WertEnde lesen
+                }
+                            
+            } // while
+            tr.Close();  // schließen der Datei
+              
+            // Dateiname andrucken
+            textBox1.Text = fName;
+            
+            return (i);         // Zeilenzahl zurückgeben
+        }
+
+        public void ResToList(int Max)
+        {
+            int i;
+
+            listBox1.Items.Clear();
+            for (i = 0; i <= Max; i++)
+            {
+                listBox1.Items.Add(Convert.ToString(GLS.GetRes(i)));
+            }
+           
+        }
+
+        // *** Ende Array-Transfer
+
+        public void DemoToGird()
+        {
+            if (dataGridView1.Rows.Count<3) {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows.Add();
+            }
+            dataGridView1.Rows[0].Cells[0].Value = 1;
+            dataGridView1.Rows[1].Cells[0].Value = 2;
+            dataGridView1.Rows[2].Cells[0].Value = 4;
+
+            dataGridView1.Rows[0].Cells[1].Value = 1;
+            dataGridView1.Rows[1].Cells[1].Value = -3;
+            dataGridView1.Rows[2].Cells[1].Value = -5;
+
+            dataGridView1.Rows[0].Cells[2].Value = 1;
+            dataGridView1.Rows[1].Cells[2].Value = -1;
+            dataGridView1.Rows[2].Cells[2].Value = -1;
+
+            dataGridView1.Rows[0].Cells[7].Value = 3;
+            dataGridView1.Rows[1].Cells[7].Value = 2;
+            dataGridView1.Rows[2].Cells[7].Value = 1;
+
+        }
+
+        public int ChkMax()
+        {
+            int i;
+            int j;
+
+            j = 0;
+            for (i = 0; i <= dataGridView1.Rows.Count-1; i++)
+            {
+                if ((dataGridView1.Rows[i].Cells[0].Value != "") && (dataGridView1.Rows[i].Cells[0].Value != " "))
+                {
+                    j = j + 1;
+                }
+            }
+            return(j-2);
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Demo-Werte setzen
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+
+            // Initialisierung GLS-Objekt
+            GLS.SetMax(2);
+            GLS.ResetErg(2);
+            GLS.InitArrays();
+            GLS.SetDemo();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Max = ChkMax();
+            GLS.SetMax(Max);
+            // Berechne
+                                                            
+            GLS.ResetErg(Max);
+            GridToArray(Max);
+            GLS.CalcGls();
+            ResToList(Max);
+            if (checkBox1.Checked==true) {
+                ArrayToGrid(Max);
+            }
+     
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Demowerte setzen
+            listBox1.Items.Clear();
+            Max = 2;
+            ClearGrid();
+            GLS.InitArrays();           
+            GLS.ResetErg(2);
+            dataGridView1.AutoResizeColumn(2);
+            DemoToGird();
+            GridToArray(2);
+            GLS.SetMax(2);
+            GLS.CalcGls();
+            ResToList(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+            // Speichern des GLS
+            Max = ChkMax();
+
+            // Speichern in Datei
+            GridToFile(Max);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            ClearGrid();
+
+            // Gls von Datei laden
+     
+            GLS.InitArrays();
+            listBox1.Items.Clear();
+
+            Max=FileToGrid(""); // Einlesen
+            GLS.SetMax(Max);
+            GLS.ResetErg(Max);
+            dataGridView1.AutoResizeColumn(Max);
+
+            GridToArray(Max);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Lösche Grid
+            textBox1.Text = "";
+            ClearGrid();
+            listBox1.Items.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                FileToGrid(textBox1.Text);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Setze Tooltip
+            System.Windows.Forms.ToolTip ToolTip1 = new System.Windows.Forms.ToolTip();
+            ToolTip1.SetToolTip(this.Info, "von Michael Graßmann");
+        }
+    }
+
+    public class CalcGLSTyp
+    {
+        double[,] fkt = new double[,] { { 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0 } };		//Fkt.-Array
+        double[] knt = new double[6] { 0, 0, 0, 0, 0, 0 };			//Konstanten-Array
+        double[] erg = new double[6] { 0, 0, 0, 0, 0, 0 };			//Ergebnis-Array
+
+        int j;					// Zeilen
+        int k2;					// Spalten
+
+        double result;
+
+        public void InitArrays()
+        {
+            int i;
+            int k;
+
+            //fkt init
+            for (k = 0; k <= 6; k++)
+            {
+                for (i = 0; i <= 6; i++)
+                {
+                    //fkt[k,i]=0;                      
+                }
+            }
+            // knt init
+            for (i = 0; i <= 6; i++)
+            {
+                //knt[i]=0;
+            }
+            //erg init
+            for (i = 0; i <= 6; i++)
+            {
+                //erg[i]=0;
+            }
+        }
+
+        public void SetDemo()
+        {
+            k2 = 2; j = 2;
+
+            fkt[0, 0] = 1;
+            fkt[1, 0] = 2;
+            fkt[2, 0] = 4;
+
+            fkt[0, 1] = 1;
+            fkt[1, 1] = -3;
+            fkt[2, 1] = -5;
+
+            fkt[0, 2] = 1;
+            fkt[1, 2] = -1;
+            fkt[2, 2] = -1;
+
+            knt[0] = 3;
+            knt[1] = 2;
+            knt[2] = 1;
+
+        }
+
+        // *** Getter-/Setter-Methoden
+        public void SetMax(int Max)
+        {
+            j = Max;
+            k2 = Max;
+        }
+
+        public double GetXVal(int x, int y)
+        {
+            return (fkt[x, y]);
+        }
+
+        public void SetXVal(int x, int y, double XVal)
+        {
+            fkt[x, y] = XVal;
+        }
+
+        public double GetKVal(int y)
+        {
+            return (knt[y]);
+        }
+
+        public void SetKVal(int y, double KVal)
+        {
+            knt[y] = KVal;
+        }
+
+
+        public double GetRes(int y)
+        {
+            return (erg[y]);
+        }
+
+        public void ResetErg(int Max)
+        {
+            int i;
+
+            for (i = 0; i <= Max; i++)
+            {
+                erg[i] = 0;
+            }
+        }
+
+        // *** Ende Getter/Setter-Methoden
+
+        public void CGls(int s)
+        {
+            int z;
+            double fakt;
+            int id;
+            z = s + 1;
+            id = 0;
+            for (z = s + 1; z <= j; z++)
+            {
+                if (fkt[z, s] != 0)
+                {
+                    fakt = (-1 * fkt[z, s]) / fkt[s, s];
+                    for (id = 0; id <= k2; id++)
+                    {
+                        fkt[z, id] = fkt[z, id] + (fkt[s, id] * fakt);
+                    }
+                    knt[z] = knt[z] + (knt[s] * fakt);
+                }
+            }
+        }
+
+        public void CFkt()
+        {
+            int z;
+            int id;
+            double sum;
+
+
+            for (z = j; z >= 0; z--)
+            {
+                sum = 0;
+                for (id = 0; id <= k2; id++)
+                {
+                    sum = sum + (erg[id] * fkt[z, id]);
+                }
+                erg[z] = (knt[z] + sum * -1) / fkt[z, z];
+                result = erg[z];
+            }
+        }
+
+        public void CalcGls()
+        {
+            int i;
+
+            for (i = 0; i <= k2; i++)
+            {
+                CGls(i);
+            }
+            CFkt();
+        }
+
+        public void ShowRes()
+        {
+            int i;
+
+            Console.WriteLine("Ergebnisse:");
+            for (i = 0; i <= k2; i++)
+            {
+                Console.WriteLine("X " + (i + 1) + " = {0:F2}", erg[i]);
+            }
+        }
+
+    } //Ende Klasse CalcGLSTyp
+
+}
